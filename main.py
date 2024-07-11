a063ece4ddc14a229d89733c854b323fb58ae7f1
Austin Cosner <atcosner@gmail.com>
2024-07-11 17:21:28 -0500

    - Refactor to use Jinja templates - WIP on job status page

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import uuid
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, make_response
 
 from processing.job_manager import JobManager
 from processing.util import set_up_root_logger
@@ -27,15 +27,17 @@ def create_job():
 def submit_job():
     if 'job-files' not in request.files or 'job-id' not in request.form:
         print('Missing job parameters')
-        return redirect(url_for('submit-job'))
+        return redirect(url_for('submit_job'))
 
     job = manager.create_job(request.form['job-id'])
     job.save_files(request.files.getlist('job-files'))
 
-    for file in request.files.getlist('job-files'):
-        print(file.filename)
+    return redirect(url_for('job_status', job_id=job.job_id))
 
-    return redirect(url_for('index'))
+
+@app.route('/job-status/<uuid:job_id>')
+def job_status(job_id: str):
+    return render_template('job_status.html', job_id=job_id)
 
 
 if __name__ == '__main__':
--- processing/job.py
+++ processing/job.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 from enum import Enum, auto
 from werkzeug.datastructures import FileStorage
 from pathlib import Path
@@ -20,26 +21,39 @@ class JobState(Enum):
 class Job:
     def __init__(self, parent_directory: Path, job_id: str):
         self.job_id: str = job_id
-        self.state: JobState = JobState.CREATED
+        self.states: list[tuple[JobState, datetime]] = [(JobState.CREATED, datetime.now())]
         self.exception: Exception | None = None
 
         # Create a working directory for ourselves
         self.working_dir: Path = parent_directory / job_id
-        self.working_dir.mkdir()
         logger.info(f'Job working directory: {self.working_dir}')
+        try:
+            self.working_dir.mkdir()
+        except Exception as e:
+            self._record_exception(e)
+
+    def _change_state(self, state: JobState) -> None:
+        self.states.append((state, datetime.now()))
+
+    def _record_exception(self, exception: Exception) -> None:
+        logger.exception('Encountered exception')
+        self.exception = exception
+        self._change_state(JobState.ERROR)
 
     def save_files(self, files: list[FileStorage]) -> None:
         for file in files:
+            # Handle empty FileStorage objects
+            if not file.filename:
+                logger.warning('Received file without name')
+                continue
+
             file_path = self.working_dir / file.filename
             logger.info(f'Saving: {file.filename} ({file_path})')
 
             try:
                 file.save(file_path)
             except Exception as e:
-                self.exception = e
-                logger.exception('Exception saving file')
-
-                self.state = JobState.ERROR
+                self._record_exception(e)
                 return
 
-        self.state = JobState.FILES_SUBMITTED
+        self._change_state(JobState.FILES_SUBMITTED)

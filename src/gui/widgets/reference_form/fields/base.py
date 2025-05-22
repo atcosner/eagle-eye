4ce78d1ae10491b5a93dc9a00ce83af629e6cb2f
Austin Cosner <atcosner@gmail.com>
2025-05-22 12:25:28 -0500

    wip

--- src/gui/widgets/reference_form/fields/base.py
+++ src/gui/widgets/reference_form/fields/base.py
@@ -1,9 +1,13 @@
+from typing import Any
+
 from PyQt6.QtCore import Qt, QRectF
-from PyQt6.QtGui import QPen, QColor, QPainter
-from PyQt6.QtWidgets import QGraphicsRectItem, QGraphicsItem, QGraphicsSimpleTextItem, QWidget, \
-    QStyleOptionGraphicsItem, QStyle
+from PyQt6.QtGui import QColor, QPainter, QCursor
+from PyQt6.QtWidgets import QGraphicsItem, QGraphicsSimpleTextItem, QWidget, \
+    QStyleOptionGraphicsItem, QStyle, QGraphicsSceneHoverEvent
 
 from src.database.fields.form_field import FormField
+
+from ..util import AnchorPoint, get_position_with_anchor
 
 
 class FieldLabel(QGraphicsSimpleTextItem):
@@ -30,30 +34,99 @@ class FieldLabel(QGraphicsSimpleTextItem):
 #         return self._field_db_id
 
 
-class BaseGraphicsItem(QGraphicsItem):
-    def __init__(self):
+class ResizeBox(QGraphicsItem):
+    def __init__(self, parent: QGraphicsItem, color: QColor, anchors: int):
+        super().__init__(parent)
+        self.color = color
+
+        self.setAcceptHoverEvents(True)
+        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
+        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsSelectable)
+
+        # determine our initial position
+        parent_rect = parent.boundingRect()
+        self.position_rect = get_position_with_anchor(parent_rect, anchors, 10)
+
+    #
+    # Qt overrides
+    #
+
+    def boundingRect(self) -> QRectF:
+        return self.position_rect
+
+    def paint(
+        self,
+        painter: QPainter,
+        option: QStyleOptionGraphicsItem,
+        widget: QWidget | None = None,
+    ) -> None:
+        pen = painter.pen()
+        pen.setColor(self.color)
+        painter.setPen(pen)
+        painter.fillRect(self.position_rect, self.color)
+
+        if self.parentItem().isSelected():
+            pen.setStyle(Qt.PenStyle.DashLine)
+            pen.setColor(QColor('black'))
+            painter.setPen(pen)
+            painter.drawRect(self.position_rect)
+
+
+class ResizableField(QGraphicsItem):
+    def __init__(self, position_rect: QRectF, color: QColor):
         super().__init__()
+        self.hovering: bool = False
+        self.selected: bool = False
+        self.position_rect = position_rect
+        self.color = color
+
+        self.setAcceptHoverEvents(True)
         self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
         self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsSelectable)
 
         # create the child items for our borders
-        self.edges = [
-            QGraphicsRectItem(QRectF(100, 100, 20, 20), self)
+        self.resize_anchors = [
+            ResizeBox(self, color, AnchorPoint.TOP | AnchorPoint.LEFT),
+            ResizeBox(self, color, AnchorPoint.TOP | AnchorPoint.RIGHT),
+            ResizeBox(self, color, AnchorPoint.TOP | AnchorPoint.H_CENTER),
+            ResizeBox(self, color, AnchorPoint.BOTTOM | AnchorPoint.LEFT),
+            ResizeBox(self, color, AnchorPoint.BOTTOM | AnchorPoint.RIGHT),
+            ResizeBox(self, color, AnchorPoint.BOTTOM | AnchorPoint.H_CENTER),
+            ResizeBox(self, color, AnchorPoint.V_CENTER | AnchorPoint.LEFT),
+            ResizeBox(self, color, AnchorPoint.V_CENTER | AnchorPoint.RIGHT),
         ]
+        for anchor in self.resize_anchors:
+            anchor.setVisible(False)
 
+    def update_cursor(self) -> None:
+        if self.hovering and self.selected:
+            self.setCursor(Qt.CursorShape.SizeAllCursor)
+        else:
+            self.setCursor(Qt.CursorShape.ArrowCursor)
 
-class BaseField(BaseGraphicsItem):
-    def __init__(self, field: FormField, color: QColor) -> None:
-        super().__init__()
-        self._field_db_id = field.id
-        self.position_rect = field.get_sub_field().visual_region.to_qt_rect()
-        self.color = color
+    #
+    # Qt overrides
+    #
 
-        self.label = FieldLabel(self, field.get_sub_field().name)
-        self.label.setPos(self.position_rect.topLeft())
+    def hoverEnterEvent(self, event: QGraphicsSceneHoverEvent) -> None:
+        self.hovering = True
+        self.update_cursor()
+        super().hoverEnterEvent(event)
 
-    def get_db_id(self) -> int:
-        return self._field_db_id
+    def hoverLeaveEvent(self, event: QGraphicsSceneHoverEvent) -> None:
+        self.hovering = False
+        self.update_cursor()
+        super().hoverEnterEvent(event)
+
+    def itemChange(self, change: QGraphicsItem.GraphicsItemChange, value: Any) -> Any:
+        if change == QGraphicsItem.GraphicsItemChange.ItemSelectedChange:
+            self.selected = value
+            self.update_cursor()
+            print(self.isSelected())
+            for anchor in self.resize_anchors:
+                anchor.setSelected(True)
+                anchor.setVisible(value)
+        return super().itemChange(change, value)
 
     def boundingRect(self) -> QRectF:
         return self.position_rect
@@ -75,3 +148,16 @@ class BaseField(BaseGraphicsItem):
             painter.setPen(pen)
             painter.drawRect(self.position_rect)
 
+
+class BaseField(ResizableField):
+    def __init__(self, field: FormField, color: QColor) -> None:
+        self.position_rect = field.get_sub_field().visual_region.to_qt_rect()
+        super().__init__(self.position_rect, color)
+
+        self._field_db_id = field.id
+
+        self.label = FieldLabel(self, field.get_sub_field().name)
+        self.label.setPos(self.position_rect.topLeft())
+
+    def get_db_id(self) -> int:
+        return self._field_db_id
--- src/gui/widgets/reference_form/util.py
+++ src/gui/widgets/reference_form/util.py
@@ -1,6 +1,6 @@
-from enum import Enum, auto
+from enum import Enum, auto, IntEnum
 
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QRectF, QPointF, QSizeF
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtWidgets import QGraphicsItem, QGraphicsItemGroup, QWidget, QStyleOptionGraphicsItem, QStyle
 
@@ -40,3 +40,37 @@ class RegionGroup(QGraphicsItemGroup):
             painter.setPen(pen)
 
         super().paint(painter, option, widget)
+
+
+class AnchorPoint(IntEnum):
+    TOP = 0x1
+    BOTTOM = 0x2
+    V_CENTER = 0x20
+    LEFT = 0x4
+    RIGHT = 0x8
+    H_CENTER = 0x10
+
+
+def get_position_with_anchor(inside_rect: QRectF, anchors: int, width: int) -> QRectF:
+    top_left_point = QPointF()
+    if anchors & AnchorPoint.TOP:
+        top_left_point.setY(inside_rect.topLeft().y() - width)
+    elif anchors & AnchorPoint.BOTTOM:
+        top_left_point.setY(inside_rect.bottomRight().y())
+    elif anchors & AnchorPoint.V_CENTER:
+        offset = (inside_rect.height() - width) // 2
+        top_left_point.setY(inside_rect.topLeft().y() + offset)
+    else:
+        raise RuntimeError('No vertical anchor specified')
+
+    if anchors & AnchorPoint.LEFT:
+        top_left_point.setX(inside_rect.topLeft().x() - width)
+    elif anchors & AnchorPoint.RIGHT:
+        top_left_point.setX(inside_rect.bottomRight().x())
+    elif anchors & AnchorPoint.H_CENTER:
+        offset = (inside_rect.width() - width) // 2
+        top_left_point.setX(inside_rect.topLeft().x() + offset)
+    else:
+        raise RuntimeError('No vertical anchor specified')
+
+    return QRectF(top_left_point, QSizeF(width, width))

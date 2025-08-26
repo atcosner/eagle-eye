d5d2994b44537005e7fd711ea0f2ed3a6d856ffe
Austin Cosner <atcosner@gmail.com>
2025-08-26 13:09:04 -0500

    fix db_setup.py to work if the DB does not exist already

--- scripts/db_setup.py
+++ scripts/db_setup.py
@@ -1,16 +1,14 @@
 from sqlalchemy.orm import Session
 
-from src.examples.fn_form_v1 import add_fn_form_v1
-from src.examples.kt_form_v8 import add_kt_form_v8
-
-from src.database import DB_ENGINE, OrmBase
 from src.util.paths import LocalPaths
 
-
+# importing the DB_ENGINE will create the DB if it needs to
 if LocalPaths.database_file().exists():
     LocalPaths.database_file().unlink()
-    OrmBase.metadata.create_all(DB_ENGINE)
 
+from src.database import DB_ENGINE
+from src.examples.fn_form_v1 import add_fn_form_v1
+from src.examples.kt_form_v8 import add_kt_form_v8
 
 with Session(DB_ENGINE) as session:
     add_kt_form_v8(session)

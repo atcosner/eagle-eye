a90ef2e755940430aa2bfbd7571274bd2f80c6d6
Austin Cosner <atcosner@gmail.com>
2025-10-31 12:22:02 -0500

    forgot processed versions of the sub circled options

--- src/database/processed_fields/processed_multi_checkbox_option.py
+++ src/database/processed_fields/processed_multi_checkbox_option.py
@@ -1,6 +1,7 @@
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, MappedAsDataclass, relationship
+from sqlalchemy.orm import Mapped, mapped_column, MappedAsDataclass, relationship, attribute_keyed_dict
 
+from .processed_sub_circled_option import ProcessedSubCircledOption
 from .. import OrmBase
 
 
@@ -14,6 +15,11 @@ class ProcessedMultiCheckboxOption(MappedAsDataclass, OrmBase):
 
     text: Mapped[str] = mapped_column(nullable=True)
     ocr_text: Mapped[str] = mapped_column(nullable=True)
+
+    circled_options: Mapped[dict[str, ProcessedSubCircledOption]] = relationship(
+        collection_class=attribute_keyed_dict("name"),
+        back_populates="processed_multi_checkbox_option",
+    )
 
     # Relationships
     multi_checkbox_option_id: Mapped[int] = mapped_column(ForeignKey("multi_checkbox_option.id"), init=False)

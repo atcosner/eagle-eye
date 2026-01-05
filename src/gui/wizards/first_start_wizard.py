ad3e690b142952b26a5ededd2b7684d4686c099b
Austin Cosner <atcosner@gmail.com>
2026-01-05 15:11:55 -0600

    wip

--- src/gui/wizards/first_start_wizard.py
+++ src/gui/wizards/first_start_wizard.py
@@ -1,15 +1,14 @@
 from PyQt6.QtWidgets import QWizard, QWidget
 
-from .first_start_pages.api_setup_page import ApiSetupPage
+from .first_start_pages.project_page import ProjectPage
 from .first_start_pages.start_page import StartPage
 
 
 class FirstStartWizard(QWizard):
     def __init__(self, parent: QWidget | None = None):
         super().__init__(parent)
-        self.setWindowTitle('Eagle Eye | Set Up Wizard')
+        self.setWindowTitle('Eagle Eye | First Start Wizard')
         self.setWizardStyle(QWizard.WizardStyle.ModernStyle)
 
         self.addPage(StartPage())
-        self.addPage(ApiSetupPage())
-        # self.addPage(AlignmentPage())
+        self.addPage(ProjectPage())
